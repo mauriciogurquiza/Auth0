af94222bcb35ab4297cb5fea834b3f1471209d34
Mauricio <mauriurquiza5@gmail.com>
2025-01-13 10:45:32 -0300

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+import './index.css';
+import App from './App';
+
+const domain = "mauriurquiza.us.auth0.com";
+const clientId = "tdAfcld1BDYfOp3oLjUgO0FG3GNsrHna";
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </StrictMode>
+);
